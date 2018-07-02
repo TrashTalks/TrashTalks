eda53b6ed519a4ca887f740a4a091fd1ede26065
Arturo Salmeron <asalmeron3@gatech.edu>
2018-07-02 15:35:41 -0400

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,11 +2,9 @@ const router = require("express").Router();
 const employeeRoutes = require("./employee");
 const mailingListRoutes = require("./mailingList");
 const materialRoutes = require("./material");
-// const nodeMailerRoutes = require("./nodeMail");
 
 //Routes
 router.use("/employee", employeeRoutes);
 router.use("/mailingList", mailingListRoutes);
 router.use("/material", materialRoutes);
-// router.use("/nodeMail",nodeMailerRoutes);
 module.exports = router;
