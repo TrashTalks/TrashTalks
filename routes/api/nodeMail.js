951d69c130b31b9bb347a4374e1be22da8019c93
Arturo Salmeron <asalmeron3@gatech.edu>
2018-07-02 15:35:20 -0400

    Delete nodeMail.js

--- routes/api/nodeMail.js
+++
@@ -1,9 +0,0 @@
-// 'use strict';
-// const router = require("express").Router();  
-// const getModel = require(`../../models/Nodemail.js`);
-
-// router.route("/sendEmail").post(getModel.read);
-// module.exports = router;
-
-
-
