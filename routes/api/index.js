84aac8d4ba10aafb6e98282a466755bc708b9c3a
Luke Chambers <lukechambers91@gmail.com>
2018-07-13 16:02:54 -0400

    updates DB

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const employeeRoutes = require("./employee");
 const mailingListRoutes = require("./mailingList");
 const materialRoutes = require("./material");
-const emailingRoutes = require("./emailing")
+const emailingRoutes = require("./emailing");
+const updateRoutes = require("./updates")
 //Routes
 router.use("/employee", employeeRoutes);
 router.use("/mailingList", mailingListRoutes);
 router.use("/material", materialRoutes);
 router.use("/emailing", emailingRoutes);
+router.use("/updates", updateRoutes);
 module.exports = router;
